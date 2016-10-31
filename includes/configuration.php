a529109bcd73294921b2e356dcc0c7f317cdacaf
ollorente <llorente_oscar@hotmail.com>
2016-10-31 18:12:10 -0500

    Add files via upload

---
+++ includes/configuration.php
@@ -0,0 +1,10 @@
+<?php
+    
+    $host = '/e-learn';
+    $tpl = 'default';
+    $pass = 'root';
+    $db = 'boot';
+    mysql_connect($server,$user,$pass);
+    mysql_select_db($db);
+    
+?>
---
+++ includes/conn.php
@@ -0,0 +1,8 @@
+<?php
+$server = 'localhost:8888';
+$user = 'root';
+$pass = 'root';
+$db = 'plataforma';
+mysql_connect($server,$user,$pass);
+mysql_select_db($db);
+?>
