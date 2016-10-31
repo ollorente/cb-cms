f8d8257f635c5b454e96f9a57ae34245a48c3f50
ollorente <llorente_oscar@hotmail.com>
2016-10-31 18:11:14 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,88 +1,66 @@
 <?php
-/**
-* @version		$Id: index.php 14401 2010-01-26 14:10:00Z louis $
-* @package		Joomla
-* @copyright	Copyright (C) 2005 - 2010 Open Source Matters. All rights reserved.
-* @license		GNU/GPL, see LICENSE.php
-* Joomla! is free software. This version may have been modified pursuant
-* to the GNU General Public License, and as distributed it includes or
-* is derivative of works licensed under the GNU General Public License or
-* other free or open source software licenses.
-* See COPYRIGHT.php for copyright notices and details.
-*/
-
-// Set flag that this is a parent file
-define( '_JEXEC', 1 );
-
-define('JPATH_BASE', dirname(__FILE__) );
-
-define( 'DS', DIRECTORY_SEPARATOR );
-
-require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' );
-require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' );
-
-JDEBUG ? $_PROFILER->mark( 'afterLoad' ) : null;
-
-/**
- * CREATE THE APPLICATION
- *
- * NOTE :
- */
-$mainframe =& JFactory::getApplication('site');
-
-/**
- * INITIALISE THE APPLICATION
- *
- * NOTE :
- */
-// set the language
-$mainframe->initialise();
-
-JPluginHelper::importPlugin('system');
-
-// trigger the onAfterInitialise events
-JDEBUG ? $_PROFILER->mark('afterInitialise') : null;
-$mainframe->triggerEvent('onAfterInitialise');
-
-/**
- * ROUTE THE APPLICATION
- *
- * NOTE :
- */
-$mainframe->route();
-
-// authorization
-$Itemid = JRequest::getInt( 'Itemid');
-$mainframe->authorize($Itemid);
-
-// trigger the onAfterRoute events
-JDEBUG ? $_PROFILER->mark('afterRoute') : null;
-$mainframe->triggerEvent('onAfterRoute');
-
-/**
- * DISPATCH THE APPLICATION
- *
- * NOTE :
- */
-$option = JRequest::getCmd('option');
-$mainframe->dispatch($option);
-
-// trigger the onAfterDispatch events
-JDEBUG ? $_PROFILER->mark('afterDispatch') : null;
-$mainframe->triggerEvent('onAfterDispatch');
-
-/**
- * RENDER  THE APPLICATION
- *
- * NOTE :
- */
-$mainframe->render();
-
-// trigger the onAfterRender events
-JDEBUG ? $_PROFILER->mark('afterRender') : null;
-$mainframe->triggerEvent('onAfterRender');
-
-/**
- * RETURN THE RESPONSE
- */
-echo JResponse::toString($mainframe->getCfg('gzip'));
+    
+    include( 'includes/conn.php' );
+    
+    session_start();
+    
+    if( isset( $_REQUEST['user'] ) ){
+        
+        $_SESSION['user'] = $U;
+        header( 'location: inicio.php' );
+        
+    }
+    
+    if( isset( $_REQUEST['U'] ) && !empty( $_REQUEST['U'] ) ){
+        
+        $U = $_REQUEST['U'];
+        $P = $_REQUEST['P'];
+        
+        $siesta = mysql_num_rows( mysql_query( "SELECT * FROM usuarios WHERE usuario = '" . $U . "' AND cb_claveusuario = '" . $P . "'" ) );
+        
+        if( $siesta == 1 ){
+            
+            $_SESSION['user'] = $U;
+            header( 'location: inicio.php' );
+            
+        } else {
+            
+            $alert = 'Usuario o contraseña incorrectos!';
+            $alert_color = 'danger';
+            
+        }
+        
+    }
+    
+    if( isset( $_REQUEST['user'] )  && !empty( $_REQUEST['user'] ) ){
+        
+        $U = $_REQUEST['user'];
+        $P = $_REQUEST['pass'];
+        $N = $_REQUEST['nombre'];
+        $F = $_FILES['foto']['name'];
+        $T = $_REQUEST['tipo'];
+        
+        $checar = mysql_num_rows( mysql_query( "SELECT * FROM usuarios WHERE usuario = '" . $U . "'" ) );
+        
+        if( $checar == 1 ){
+            
+            $alert = 'Ya estás registrado!';
+            $alert_color = 'danger';
+            
+        } else {
+            
+            mysql_query( "INSERT INTO usuarios VALUES('" . $U . "', '" . $P . "', '" . $N . "', '" . $U . "', '" . $T . "')" );
+            move_uploaded_file( $_FILES['foto']['tmp_name'], "images/avataruser/" . $U . ".jpg" );
+            header( 'location: index.php' );
+            
+            $alert = 'Se registró con éxito!';
+            $alert_color = 'success';
+            
+        }
+        
+    }
+    
+    include( 'includes/configuration.php' );
+    include( 'tmpl/default/index.php' );
+    
+?>
