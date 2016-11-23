0ea79591458267bf21362e2ce4f848459fed115e
ollorente <llorente_oscar@hotmail.com>
2016-11-23 14:09:50 -0500

    Update conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -1,10 +1,10 @@
 <?php
 
   $server = 'localhost:8888';
-  $user = 'root';
-  $pass = 'root';
-  $db = 'plataforma';
-  mysql_connect( $server,$user,$pass,$db );
-  mysql_select_db($db);
+  $user   = 'root';
+  $pass   = 'root';
+  $db     = 'plataforma';
+
+  mysql_connect( $server, $user, $pass, $db );
 
 ?>
