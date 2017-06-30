da046ee810b6b24d5149dee9c8e0589371184f52
ollorente <llorente_oscar@hotmail.com>
2017-06-30 14:03:26 -0500

    Se quitaron los espacios entre las funciones
    En este paso se quitaron espacio existentes el las filas sin código y la que había a los lados de los paréntesis y puntos.

--- index.php
+++ index.php
@@ -1,66 +1,46 @@
 <?php
-    
-    include( 'includes/conn.php' );
-    
-    session_start();
-    
-    if( isset( $_REQUEST['user'] ) ){
-        
-        $_SESSION['user'] = $U;
-        header( 'location: inicio.php' );
-        
-    }
-    
-    if( isset( $_REQUEST['U'] ) && !empty( $_REQUEST['U'] ) ){
-        
-        $U = $_REQUEST['U'];
-        $P = $_REQUEST['P'];
-        
-        $siesta = mysql_num_rows( mysql_query( "SELECT * FROM usuarios WHERE usuario = '" . $U . "' AND cb_claveusuario = '" . $P . "'" ) );
-        
-        if( $siesta == 1 ){
-            
-            $_SESSION['user'] = $U;
-            header( 'location: inicio.php' );
-            
-        } else {
-            
-            $alert = 'Usuario o contraseña incorrectos!';
-            $alert_color = 'danger';
-            
-        }
-        
-    }
-    
-    if( isset( $_REQUEST['user'] )  && !empty( $_REQUEST['user'] ) ){
-        
-        $U = $_REQUEST['user'];
-        $P = $_REQUEST['pass'];
-        $N = $_REQUEST['nombre'];
-        $F = $_FILES['foto']['name'];
-        $T = $_REQUEST['tipo'];
-        
-        $checar = mysql_num_rows( mysql_query( "SELECT * FROM usuarios WHERE usuario = '" . $U . "'" ) );
-        
-        if( $checar == 1 ){
-            
-            $alert = 'Ya estás registrado!';
-            $alert_color = 'danger';
-            
-        } else {
-            
-            mysql_query( "INSERT INTO usuarios VALUES('" . $U . "', '" . $P . "', '" . $N . "', '" . $U . "', '" . $T . "')" );
-            move_uploaded_file( $_FILES['foto']['tmp_name'], "images/avataruser/" . $U . ".jpg" );
-            header( 'location: index.php' );
-            
-            $alert = 'Se registró con éxito!';
-            $alert_color = 'success';
-            
-        }
-        
-    }
-    
-    include( 'includes/configuration.php' );
-    include( 'tmpl/default/index.php' );
-    
+
+	include('includes/conn.php');
+
+	session_start();
+
+	if(isset($_REQUEST['user'])){
+		$_SESSION['user']=$U;
+		header('location:inicio.php');
+	}
+
+	if(isset($_REQUEST['U']) && !empty($_REQUEST['U'])){ 
+		$U=$_REQUEST['U'];
+		$P=$_REQUEST['P'];
+		$siesta=mysql_num_rows(mysql_query("SELECT * FROM usuarios WHERE usuario = '".$U."' AND cb_claveusuario = '".$P."'"));
+		if($siesta==1){
+			$_SESSION['user']=$U;
+			header('location:inicio.php');
+		} else {
+			$alert='Usuario o contraseña incorrectos!';
+			$alert_color='danger';
+		}
+	}
+
+	if(isset($_REQUEST['user'])  && !empty($_REQUEST['user'])){
+		$U=$_REQUEST['user'];
+		$P=$_REQUEST['pass'];
+		$N=$_REQUEST['nombre'];
+		$F=$_FILES['foto']['name'];
+		$T=$_REQUEST['tipo'];
+		$checar=mysql_num_rows(mysql_query("SELECT * FROM usuarios WHERE usuario = '".$U."'"));
+		if($checar==1){
+			$alert='Ya estás registrado!';
+			$alert_color='danger';
+		} else {
+			mysql_query("INSERT INTO usuarios VALUES('".$U."', '".$P."', '".$N."', '".$U."', '".$T."')");
+			move_uploaded_file($_FILES['foto']['tmp_name'], "images/avataruser/".$U.".jpg");
+			header('location:index.php');
+			$alert='Se registró con éxito!';
+			$alert_color='success';
+		}
+	}
+
+	include('includes/configuration.php');
+	include('tmpl/default/index.php');
 ?>
